45115a37be4105966cb6f36111ab03bb9e973212
Maygli <mmaygli@gmail.com>
2022-02-14 08:22:45 +0300

    NodeJS server for debug was added

---
+++ WiFiSwitch/firmware/debug/nodejs_server/test_server.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+function getBoardConfig()
+{
+  var aBoardInfo = {
+    "board_name": "Datcha WiFi switch",
+    "board_hw_version": "v1.0.0",
+    "board_sw_version": "v2.1.3",
+    "board_desc": "Datcha WiFi switch",
+    "manufacturer": "Maygli <a href=\"mailto:mmaygli@gmail.com\">mmaygli@gmail.com</a>",
+    "copyright": "&copy; Maygli 2021",
+    "devices": "wifi_switch"
+  };
+  return JSON.stringify(aBoardInfo)
+}
+
+function getSwitchSettings()
+{
+  var aWiFiSettings = {
+    "on_brightness": 47,
+    "off_brightness": 36,
+    "sound": "off",
+    "style": 1,
+  };
+  return JSON.stringify(aWiFiSettings)
+}
+
+function getWiFiSettings(){
+  var aWiFiSettings = {
+    "is_st" : "on", 
+    "ap_ssid":"Test",
+    "ap_pwd":"11111111111111111",
+    "ap_fixed_ip":"off",
+    "ap_ip":"192.168.1.11",
+    "ap_netmask":"255.255.255.0",
+    "ap_gateway":"192.168.1.1",
+    "st_ssid":"MaygliHome",
+    "st_pwd":"1",
+    "st_fixed_ip":"on",
+    "st_ip": "192.168.1.70",
+    "st_netmask":"255.255.0.0",
+    "st_gateway":"192.168.2.2",
+    "st_attempts": "5"
+  };
+  return JSON.stringify(aWiFiSettings)
+}
+
+app.use(fileUpload())
+app.post('/internal_upload', function(req, res) {
+  console.log(req.files);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  res.send('File uploaded!');
+
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+/*  let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });*/
+});
+
+
+app.use(express.static('../../data'));
+
+app.get("/switch_settings", function(request, response){
+    response.send(getSwitchSettings());
+});
+
+app.use(bodyParser.json());
+
+app.post("/switch_settings", function(request, response){
+     console.log("Set switch parameters");
+     console.log(request.body);
+});
+
+app.get("/wifi_settings", function(request, response){
+    response.send(getWiFiSettings());
+});
+
+app.post("/wifi_settings", function(request, response){
+     console.log("Set wifi parameters");
+     console.log(request.body);
+     response.send("Ok");
+});
+
+app.get("/board_info", function(request, response){
+    response.send(getBoardConfig());
+});
+app.listen(3000);
